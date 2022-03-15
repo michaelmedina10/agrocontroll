7d271d36e2b4b80499cb11da2c8d9e73cb3650d6
Michael Medina <michael.cunha97@hotmail.com>
2022-03-15 14:57:29 -0300

    Adicionando o popup para editar e ecluir um registro

--- frontend/agrocontroll/src/components/FarmerDialog.vue
+++ frontend/agrocontroll/src/components/FarmerDialog.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-dialog v-model="value">
+  <v-dialog v-model="value" max-width="800px" persistent>
     <v-card>
       <v-card-title>
         <h1>Atualizar Registro</h1>
@@ -9,23 +9,89 @@
         </v-btn>
       </v-card-title>
       <v-card-text>
-        Formulário
-        <slot name="item"></slot>
+        <v-form>
+          <v-container>
+            <v-row>
+              <v-col cols="12" sm="6">
+                <v-text-field
+                  v-model="editedItem.nome"
+                  label="Nome"
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6">
+                <v-text-field
+                  v-model="editedItem.estado"
+                  label="Estado"
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6">
+                <v-text-field
+                  v-model="editedItem.email"
+                  label="E-mail"
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="4">
+                <v-text-field
+                  v-model="editedItem.idade"
+                  label="Idade"
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6">
+                <v-text-field
+                  v-model="editedItem.pesticida"
+                  label="Pesticida"
+                  required
+                ></v-text-field>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-form>
       </v-card-text>
       <v-card-actions>
         <v-spacer></v-spacer>
-        <v-btn color="primary"> Atualizar </v-btn>
+        <v-btn color="primary" @click="updateFarmer"> Atualizar </v-btn>
         <v-btn class="ml-3" @click="closeDialog"> Cancelar </v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
 </template>
 <script>
+import Axios from "axios";
 export default {
   props: ["value"],
+  data() {
+    return {};
+  },
+  computed: {
+    editedItem() {
+      return { ...this.$store.state.farmer };
+    },
+  },
   methods: {
     closeDialog() {
       this.$emit("input", false);
+    },
+    updateFarmer() {
+      const itemsToBeEdited = { ...this.editedItem };
+      itemsToBeEdited.idade = parseInt(itemsToBeEdited.idade);
+      Axios.put(
+        `${process.env.VUE_APP_BASEURL}/agrousers/${itemsToBeEdited.id}`,
+        itemsToBeEdited
+      )
+        .then((_) => {
+          const editedIndex = this.$store.state.farmes.indexOf(
+            this.$store.state.farmes.filter(
+              (farmer) => farmer.id == itemsToBeEdited.id
+            )[0]
+          );
+          this.$store.state.farmes[editedIndex] = itemsToBeEdited;
+        })
+        .catch((err) => res.send(err));
+      this.closeDialog();
     },
   },
 };
--- frontend/agrocontroll/src/components/TableRegister.vue
+++ frontend/agrocontroll/src/components/TableRegister.vue
@@ -1,13 +1,47 @@
 <template>
   <div>
-    <v-data-table :headers="headers" :items="farmes">
+    <v-data-table :headers="headers" :items="farmesList">
       <template #[`item.acao`]="{ item }">
-        <v-btn small fab color="warning" @click="update(item)">
+        <v-btn small fab color="warning" @click.stop="update(item)">
           <v-icon>mdi-pencil</v-icon>
         </v-btn>
-        <v-btn small fab color="error" class="ml-3" @click="remove(item)">
-          <v-icon>mdi-delete</v-icon>
-        </v-btn>
+        <v-dialog
+          v-model="dialogRemoveFarmer"
+          max-width="500px"
+          persistent
+          :retain-focus="false"
+        >
+          <template v-slot:activator="{ on }">
+            <v-btn
+              small
+              fab
+              color="error"
+              class="ml-3"
+              @click="DialogOfRemove(item)"
+              v-on="on"
+            >
+              <v-icon>mdi-delete</v-icon>
+            </v-btn>
+          </template>
+          <v-card>
+            <v-card-title>
+              <h1>Remover Registro</h1>
+            </v-card-title>
+            <v-card-text>
+              <span
+                >O Registro com ID:{{ farmer.id }} Nome:{{ farmer.nome }} será
+                removido</span
+              >
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="primary" @click="remove"> Remover </v-btn>
+              <v-btn class="ml-3" @click="dialogRemoveFarmer = false">
+                Cancelar
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
       </template>
     </v-data-table>
     <FarmerDialog v-model="dialog" />
@@ -23,6 +57,7 @@ export default {
   data() {
     return {
       dialog: false,
+      dialogRemoveFarmer: false,
       headers: [
         { text: "Nome", value: "nome" },
         { text: "Estado", value: "estado" },
@@ -31,27 +66,51 @@ export default {
         { text: "Pesticida", value: "pesticida" },
         { text: "Ações", value: "acao", sortable: false },
       ],
-      farmes: [],
       farmer: {},
     };
   },
   methods: {
     loadFarmes() {
       Axios.get(`${process.env.VUE_APP_BASEURL}/agrousers`)
-        .then((res) => (this.farmes = res.data))
+        .then((res) => (this.$store.state.farmes = res.data))
         .catch((err) => res.send(err));
     },
 
     update(farmer) {
-      this.farmer = farmer;
+      this.$store.state.farmer = farmer;
       this.dialog = true;
     },
-    remove(farmer) {
+    DialogOfRemove(farmer) {
       console.log("Removendo...");
+      this.dialogRemoveFarmer = true;
+      this.farmer = farmer;
+    },
+    remove() {
+      Axios.delete(
+        `${process.env.VUE_APP_BASEURL}/agrousers/${this.farmer.id}`
+      ).then((_) => {
+        const editedIndex = this.$store.state.farmes.indexOf(
+          this.$store.state.farmes.filter(
+            (farmer) => farmer.id == this.farmer.id
+          )[0]
+        );
+        this.$store.state.farmes.splice(editedIndex);
+        this.dialogRemoveFarmer = false;
+      });
     },
   },
   mounted() {
     this.loadFarmes();
+  },
+  computed: {
+    farmesList() {
+      return this.$store.state.farmes;
+    },
+  },
+  watch: {
+    farmesList() {
+      this.loadFarmes();
+    },
   },
 };
 </script>
