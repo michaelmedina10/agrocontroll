ce951067a03e6f2f53b3df0d08ac6658641999f4
Michael Medina <68739172+michaelmedina10@users.noreply.github.com>
2022-03-26 09:51:21 -0300

    Removendo o token bearer de teste

--- frontend/agrocontroll/src/main.js
+++ frontend/agrocontroll/src/main.js
@@ -6,9 +6,6 @@ import vuetify from "./plugins/vuetify";
 import Axios from "axios";
 
 Vue.config.productionTip = false;
-// Só teste de autenticacao, apagar depois
-// Axios.defaults.headers.common["Authorization"] =
-//   "bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MjUsIm5hbWUiOiJtaWNoYWVsIiwiZW1haWwiOiJtaWNoYWVsLm1lZGluYUBnbWFpbC5jb20iLCJhZG1pbiI6IjEiLCJpYXQiOjE2NDgyMTAxNjcsImV4cCI6MTY0ODQ2OTM2N30.ZLDJM_eT6uqcp7K4lWyMJ0OkLmRZh3CcGuAbkHBe_Tc";
 new Vue({
   router,
   store,
